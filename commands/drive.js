282e3e66adce8a659d8a73c4cfdbee71fed134fd
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-06-29 09:25:55 -0400

    Updated JSON Write File Method

--- commands/drive.js
+++ commands/drive.js
@@ -1,63 +1,66 @@
+
+
 const Discord = require("discord.js");
 const fetch = require("node-fetch");
 const axios = require("axios");
 const fs = require("fs");
 const commandLog = require("./command-logs.json");
 
-module.exports.run = async (client, msg, args) => {
-  if (!args[0])
-    msg.reply(
-      "Syntaxe : `;drive (Pseudo en jeu) (Item commandé) (Quantité en stacks)`"
-    );
+module.exports.run = async(client, msg, args) => {
+	if (!args[0])
+		msg.reply(
+			"Syntaxe : `;drive (Pseudo en jeu) (Item commandé) (Quantité en stacks)`"
+		);
 
-  const discordUser = msg.member.user.tag;
+	const discordUser = msg.member.user.tag;
 
-  if(!commandLog[msg.member.user.tag]) {
-    commandLog[discordUser] = {
-      commandes: 0
-    };
-  }
+	const data = {
+		email: "TrkqfcYTzCkSzT4W5FLcFNnYsA42ckgIn5GNeoP8BORujj+PYUjRBrDl5ofOU/vh5KqSH+a1CbEkKcVc0SFQTN6SohKih/r4zlu4Vgh9XiheRMTMgLiD8NeUo6aAPu8V",
+		form: {
+			title: "[BOT LIDL] Mobirise Form",
+			data: [
+				["Pseudo In-Game", `${args[0]}`],
+				["Item commandé", `${args[1]}`],
+				["Nombre commandé", `${args[2]}`],
+				["Site", `lidl.mrholly70.fr`],
+				["Pseudo Discord", `${discordUser}`],
+			],
+		},
+	};
 
-  const data = {
-    email:
-      "TrkqfcYTzCkSzT4W5FLcFNnYsA42ckgIn5GNeoP8BORujj+PYUjRBrDl5ofOU/vh5KqSH+a1CbEkKcVc0SFQTN6SohKih/r4zlu4Vgh9XiheRMTMgLiD8NeUo6aAPu8V",
-    form: {
-      title: "[BOT LIDL] Mobirise Form",
-      data: [
-        ["Pseudo In-Game", `${args[0]}`],
-        ["Item commandé", `${args[1]}`],
-        ["Nombre commandé", `${args[2]}`],
-        ["Site", `lidl.mrholly70.fr`],
-        ["Pseudo Discord", `${discordUser}`],
-      ],
-    },
-  };
+	axios
+		.post("https://formoid.net/api/push", data)
+		.then((res) => {
+			console.log(`Status: ${res.status}`);
+			console.log("Body: ", res.data);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
 
-  axios
-    .post("https://formoid.net/api/push", data)
-    .then((res) => {
-      console.log(`Status: ${res.status}`);
-      console.log("Body: ", res.data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+	msg.reply("Merci d'avoir passé une commande avec Lidl Drive !");
 
-  msg.reply("Merci d'avoir passé une commande avec Lidl Drive !");
+	let commandDrive = `Vous avez commandé sous le nom de ${args[0]} ${args[2]} de ${args[1]}`
 
-  let commandDrive = `Vous avez commandé sous le nom de ${args[0]} ${args[2]} de ${args[1]}`
+	var fs = require("fs");
+	var sampleObject = {
+		user: `${discordUser}` {
+			name: `${args[0]}`,
+			item: `${args[1]}`,
+			quantity: `${args[2]}`
+		}
+	};
 
-  let pastCommand = commandLog[discordUser]
-
-  let jsonCommand = `${pastCommand} ${commandDrive}`
-
-  if(commandLog[discordUser]) {
-    commandLog[discordUser] = {
-      commandes: jsonCommand
-    };
-  }
+	fs.writeFile("./command-log.json", JSON.stringify(sampleObject, null, 4), (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		};
+		console.log("File has been created");
+	});
 };
 
 module.exports.help = {
-  name: "drive",
+	name: "drive",
 };
+
