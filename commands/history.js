bbba3f668d534c8ce56d57c4181b1c722e0de412
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-06-30 17:54:02 -0400

    Update history.js

--- commands/history.js
+++ commands/history.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const fs = require("fs");
 
 (module.exports.run = async (client, msg, args) => {
-  const discordUser = args[0];
+  const discordUser = msg.author.user.id;
 
   const log1Time = await fetch(
     `https://google-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
