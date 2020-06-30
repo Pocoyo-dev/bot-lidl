54d156f25400c636e1b96e9b240f0c5709e194f4
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-06-30 13:01:02 -0400

    Update history.js

--- commands/history.js
+++ commands/history.js
@@ -2,34 +2,30 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 const axios = require("axios");
 const fs = require("fs");
-const sheetsu = require("sheetsu-node");
-const sheetClient = sheetsu({
-  address: "https://sheetsu.com/apis/v1.0su/0600bbb58ade",
-});
 
 (module.exports.run = async (client, msg, args) => {
   const discordUser = msg.member.user.id;
 
   const log1Time = await fetch(
-    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
+    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
     .then((json) => json.rows.horodateur);
 
   const log1Pseudo = await fetch(
-    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
+    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
     .then((json) => json.rows.pseudo);
 
   const log1Quantity = await fetch(
-    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
+    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
     .then((json) => json.rows.quantité);
 
   const log1Item = await fetch(
-    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
+    `https://google-form-to-json.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}.herokuapp.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
     .then((json) => json.rows.pseudo);
