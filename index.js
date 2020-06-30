af29695b10c5b795fdaaae3a72a6e4e3315a5626
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-06-30 11:43:07 -0400

    Added time to log

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const config = require("./config.json")
 
 client.commands = new Discord.Collection();
 
+let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+
 fs.readdir("./commands/", (err, files) => {
   if (err) console.log(err);
 
@@ -22,7 +31,7 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}! The time is` + year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
 });
 
 const commandFiles = fs
