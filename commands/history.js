56785e1a0f34e26bd9023a6fa7a8c7a748caad09
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-06-30 19:36:21 -0400

    Update history.js

--- commands/history.js
+++ commands/history.js
@@ -4,33 +4,33 @@ const axios = require("axios");
 const fs = require("fs");
 
 (module.exports.run = async (client, msg, args) => {
-  const discordUser = msg.author.id;
+  const discordUser = args[0];
 
   const log1Time = await fetch(
     `http://gsx2json.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
-    .then((json) => json.rows.horodateur);
+    .then((json) => json.columns.horodateur);
 
   const log1Pseudo = await fetch(
     `http://gsx2json.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
-    .then((json) => json.rows.pseudo);
+    .then((json) => json.columns.pseudo);
 
   const log1Quantity = await fetch(
     `http://gsx2json.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
-    .then((json) => json.rows.quantité);
+    .then((json) => json.columns.quantité);
 
   const log1Item = await fetch(
     `http://gsx2json.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
-    .then((json) => json.rows.pseudo);
-  console.log(log1Item + log1Pseudo + log1Quantity + log1Time);
-  console.log(`${discordUser}`);
+    .then((json) => json.columns.pseudo);
+
+
   msg.reply(
     `Vous avez commandé **${log1Quantity}** de **${log1Item}** sous le pseudo **${log1Pseudo}** le **${log1Time}**`
   );
