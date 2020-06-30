627d13e13fcda433c1aa737aa78b757d558f21e5
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-06-30 12:05:14 -0400

    Create idHistory.js

---
+++ commands/idHistory.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const axios = require("axios");
+const fs = require("fs");
+const sheetsu = require("sheetsu-node");
+const sheetClient = sheetsu({
+  address: "https://sheetsu.com/apis/v1.0su/0600bbb58ade",
+});
+
+module.exports.run = async (client, msg, args) => {
+  const discordUserID = args[0];
+
+  sheetClient.read({ search: { DISCORD: discordUser } }).then(
+    function (data) {
+      console.log(data);
+      const dataMSG = data;
+
+      var msgData2 = `${dataMSG}`;
+      msgData2 = msgData2.replace("[", " ");
+      msgData2 = msgData2.replace("]", " ");
+      msgData2 = msgData2.replace("},{", ",");
+      msgData2 = msgData2.replace("},{", ",");
+
+      msg.reply(
+        `Vous avez commandé **${finalMessage.QUANTITY}** de **${finalMessage.ITEM}** sous le pseudo **${finalMessage.PSEUDO}** le **${finalMessage.Horodateur}**`
+      );
+    },
+    function (err) {
+      console.log(err);
+      msg.reply(
+        "Désolé mais un bug est survenu du côté du site ! Nous ne pouvons rien y faire, réessaie plus tard :("
+      );
+    }
+  );
+};
+
+module.exports.help = {
+  name: "idHistory",
+};
