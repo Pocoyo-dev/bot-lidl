c593eaf5d9de7eccad567c448550aa6c4064a27b
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-06-30 10:05:25 -0400

    Reaction

--- commands/drive.js
+++ commands/drive.js
@@ -44,6 +44,7 @@ module.exports.run = async (client, msg, args) => {
       { name: 'Afin de confirmer votre commande, veuillez cliquer sur le lien ci-dessous', value: `[Lien de confirmation](https://docs.google.com/forms/d/e/1FAIpQLSePBqQqwbD42wd6A41ATDzKaxFfeTRWuHgkgWL3AvJkqmcP2Q/formResponse?usp=pp_url&entry.1818559927=${args[0]}&entry.86871255=${args[1]}&entry.46215118=${args[2]}&entry.740390018=${discordUser}&submit=Submit "Lien de confirmation")` },
     )  
   msg.reply(confirmationEmbed);
+  msg.react("👍");
 
   let commandDrive = `Vous avez commandé sous le nom de ${args[0]} ${args[2]} de ${args[1]}`;
 };
