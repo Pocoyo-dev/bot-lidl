aa010396f41356352eca67b366c89660e12cf0bf
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-07-01 10:21:17 -0400

    Added some things

---
+++ commands/help.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const axios = require("axios");
+const fs = require("fs");
+
+module.exports.run = async (client, msg, args) => {
+  const helpEmbed = new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Page d<aide")
+    .setDescription("Some description here")
+    .addFields(
+      { name: "~drive **[PSEUDO]** **[ITEM]** **[QUANTITÉ]**", value: "Envoie une commande au **Lidl Drive**", inline: true },
+      { name: "~history **[PSEUDO || DATE]**", value: "Affiche votre historique de commandes", inline: true }
+    )
+    .setFooter("Fait avec amour par Pocoyo ;3");
+
+  channel.send(helpEmbed);
+};
+
+module.exports.help = {
+  name: "history",
+};
--- commands/history.js
+++ commands/history.js
@@ -11,23 +11,51 @@ const fs = require("fs");
   )
     .then((res) => res.json())
     .then((json) => json.columns.pseudo);
+  let logPseudoMSG = logItem.replace(",", /r/n);
 
   const logQuantity = await fetch(
     `http://gsx2json.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
     .then((json) => json.columns.quantity);
+  let logQuantityMSG = logItem.replace(",", /r/n);
 
   const logItem = await fetch(
     `http://gsx2json.com/api?id=1LGEDEHkI0WBgzxtG7UidGO-m-gn9qNIPQk4o0jOVhLk&q=${discordUser}`
   )
     .then((res) => res.json())
     .then((json) => json.columns.item);
+  let logItemMSG = logItem.replace(",", /r/n);
 
+  if (logPseudo === "undefined") {
+    msg.reply(
+      "Il n'y a aucune commandes à ce nom ! Veuillez réessayer ! Si vous pensez que ceci est une erreur veuillez contacter un administrateur du serveur."
+    );
+  }
+  const historyEmbed = new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Page d<aide")
+    .setDescription("Some description here")
+    .addFields(
+      {
+        name: "PSEUDO",
+        value: `${logPseudoMSG}`,
+        inline: true,
+      },
+      {
+        name: "ITEM",
+        value: `${logItemMSG}`,
+        inline: true,
+      },
+      {
+        name: "QUANTITÉ",
+        value: `${logQuantityMSG}`,
+        inline: false,
+      }
+    )
+    .setFooter("Fait avec amour par Pocoyo ;3");
 
-  msg.reply(
-    `Les commandes ont étées passées de **${logQuantity}** de **${logItem}** sous le pseudo **${logPseudo}**`
-  );
+  channel.send(historyEmbed);
 }),
   function (err) {
     console.log(err);
