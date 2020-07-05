6a7941dfaa97bf317988adc3d6983aec8729db7e
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-07-05 13:16:07 -0400

    Update drive.js

--- commands/drive.js
+++ commands/drive.js
@@ -38,8 +38,8 @@ module.exports.run = async (client, msg, args) => {
       console.error(err);
     });
 
-  const data = {
-    data: [
+  const dataLog = {
+    dataLog: [
       { "value1" : `${args[0]}`, "value2" : `${args[1]}`, "value3" : `${args[2]}` } 
     ],
   };
@@ -47,7 +47,7 @@ module.exports.run = async (client, msg, args) => {
   axios
     .post(
       "https://maker.ifttt.com/trigger/addLidlCommand/with/key/jTkvYSawr5etL9ngeCBnMrCMsoUCNEO3oIuEpBEbXmg",
-      data
+      dataLog
     )
     .then((res) => {
       console.log(`Status: ${res.status}`);
