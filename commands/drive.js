114cbc22d04af81184f791d4ebeecf08d52d6f59
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-07-07 14:46:07 -0400

    Update drive.js

--- commands/drive.js
+++ commands/drive.js
@@ -54,11 +54,11 @@ module.exports.run = async (client, msg, args) => {
   const confirmationEmbed = new Discord.MessageEmbed()
     .setColor("#0099ff")
     .setTitle("Votre commande est passée !")
-    .addFields({
+  /* .addFields({
       name:
         "Afin de confirmer votre commande, veuillez cliquer sur le lien ci-dessous",
       value: `[Lien de confirmation](https://docs.google.com/forms/d/e/1FAIpQLSePBqQqwbD42wd6A41ATDzKaxFfeTRWuHgkgWL3AvJkqmcP2Q/formResponse?usp=pp_url&entry.1818559927=${args[0]}&entry.86871255=${args[1]}&entry.46215118=${args[2]}&entry.740390018=${discordUser}&submit=Submit "Lien de confirmation")`,
-    });
+    });*/
   msg.reply(confirmationEmbed).then(function (msg) {
     msg.react("👍");
   });
