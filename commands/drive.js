9b9baf94266cf95ad9a1f7bde8a849abe3e3db57
PocoyoBots <67566122+PocoyoBots@users.noreply.github.com>
2020-07-08 19:49:38 -0400

    Package nodemailer added

--- commands/drive.js
+++ commands/drive.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const fs = require("fs");
 const request = require("request");
 const itemList = require("../itemlist.json");
+const nodemailer = require("nodemailer")
 
 module.exports.run = async (client, msg, args) => {
   if (!args[0]) {
@@ -32,7 +33,42 @@ module.exports.run = async (client, msg, args) => {
 
       const discordUser = msg.member.user.id;
 
-      const data = {
+      async function emailSend() {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let tempAccount = await nodemailer.createTestAccount();
+      
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: tempAccount.user, // generated ethereal user
+            pass: tempAccount.pass, // generated ethereal password
+          },
+        });
+      
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+          from: '"COMMANDE LIDL J2C" <commandes@lidlj2c.fr>', // sender address
+          to: "bobafak555@wonwwf.com", // list of receivers
+          subject: "COMMANDE BOT LIDL J2C", // Subject line
+          text: "Bonjour ! Une commande a été passée via le bot ! \n Pseudo En Jeu :" + args[0] + "\n Item :" + args[1] + "\n Quantité :" + args[2] + "\n Discord" + discordUser + "\n \n Bonne chance !", // plain text body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      
+      }
+      
+      emailSend().catch(console.error);
+
+/*      const data = {
         email:
         "TrkqfcYTzCkSzT4W5FLcFNnYsA42ckgIn5GNeoP8BORujj+PYUjRBrDl5ofOU/vh5KqSH+a1CbEkKcVc0SFQTN6SohKih/r4zlu4Vgh9XiheRMTMgLiD8NeUo6aAPu8V",
         form: {
@@ -55,7 +91,7 @@ module.exports.run = async (client, msg, args) => {
         })
         .catch((err) => {
           console.error(err);
-        });
+        }); */
 
       const confirmationEmbed = new Discord.MessageEmbed()
         .setColor("#0099ff")
